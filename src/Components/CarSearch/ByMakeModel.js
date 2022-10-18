dabfadf36c0d011350fbe4893ec2d8ce9f8d44bd
Spider-Man <abeernaeem6@gmail.com>
2022-10-18 00:19:34 -0500

    added seeds

--- src/Components/CarSearch/ByMakeModel.js
+++ src/Components/CarSearch/ByMakeModel.js
@@ -1,5 +1,15 @@
 import React from "react";
-import Cars from "./seed";
+import {
+  Cars,
+  Camry,
+  Corolla,
+  Highlander,
+  Edge,
+  Escape,
+  Focus,
+  Accord,
+  Civic,
+} from "./seed";
 import { useState } from "react";
 import { useEffect } from "react";
 
@@ -8,40 +18,160 @@ const ByMakeModel = () => {
   console.log(Models);
 
   const [disable, setDisable] = useState(true);
-  const [Make, setMake] = useState({Make: null});
+  const [Make, setMake] = useState({ Make: null });
+  const [Display, setDisplay] = useState("");
 
   useEffect(() => {
-
-    if(Make.Make) {
-      setDisable(false);}
-
-    }, [Make]);
+    if (Make.Make) {
+      setDisable(false);
+      setDisplay("")
+    }
+  }, [Make]);
   const HandleChange = (event) => {
     const Index = event.target.selectedIndex;
-    setMake({Make:Object.values(Cars)[Index]});
-    
-    
+    setMake({ Make: Object.values(Cars)[Index - 1] });
+
     console.log(Object.values(Cars)[Index]);
-    console.log(Make)
-};
+    console.log(Make);
+  };
+
+  const HandleSubmit = (event) => {
+    const chosenCar = event.target.value;
+
+    switch (chosenCar) {
+      case "Camry":
+        setDisplay(
+          Camry.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      case "Corolla":
+        setDisplay(
+          Corolla.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      case "Highlander":
+        setDisplay(
+          Highlander.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      case "Edge":
+        setDisplay(
+          Edge.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      case "Escape":
+        setDisplay(
+          Escape.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      case "Focus":
+        setDisplay(
+          Focus.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      case "Accord":
+        setDisplay(
+          Accord.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      case "Civic":
+        setDisplay(
+          Civic.map((car) => (
+            <img
+              src={car}
+              width="200"
+              height="150"
+              style={{ display: "block", margin: "20px" }}
+            ></img>
+          ))
+        );
+        break;
+      default:
+        setDisplay("");
+    }
+  };
 
   return (
     <>
       <form>
         <select placeholder="All Makes" name="Makes" onChange={HandleChange}>
-          {Models.map(model => 
-            <option value={model} >{model}</option>
+          <option value="" disabled selected>
+            All Makes
+          </option>
+
+          {Models.map((model) => (
+            <option value={model}>{model}</option>
+          ))}
+        </select>
+
+        <select
+          disabled={disable}
+          placeholder="All Models"
+          name="Models"
+          onChange={HandleSubmit}
+        >
+          <option value="" disabled selected>
+            All Models
+          </option>
+
+          {Make.Make ? (
+            Make.Make.map((model) => <option value={model}>{model}</option>)
+          ) : (
+            <option></option>
           )}
         </select>
-
-        <select disabled={disable} placeholder="All Models" name="Models">
-
-
-          {Make.Make ? Make.Make.map(model => 
-            <option value={model}>{model}</option>
-          ): <option></option>}
-        </select>
       </form>
+
+      {Display}
     </>
   );
 };
