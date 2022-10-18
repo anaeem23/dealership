fb8194e00f967d3c657692756efbce325b0b1618
Spider-Man <abeernaeem6@gmail.com>
2022-10-18 17:35:30 -0500

    added cards

--- src/Components/CarSearch/seed.js
+++ src/Components/CarSearch/seed.js
@@ -1,27 +1,88 @@
 export const Cars = {
-    Toyota: ["Camry", "Corolla", "Highlander"],
-    Ford: ["Edge", "Escape", "Focus",],
-    Honda: ["Accord", "Civic"]
-}
+  Toyota: ["Camry", "Corolla", "Highlander"],
+  Ford: ["Edge", "Escape", "Focus"],
+  Honda: ["Accord", "Civic"],
+};
 
-export const Camry = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSicayxK-zSQKO2q0gvy_27RbfYY7aOhLUtQ&usqp=CAU",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT49kT1efPvyvHqPRVaoF67NhM3r4u1geV1Wg&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQle3P7smI3CT9kV4U5tW_g_5hh1F1GAXvcEA&usqp=CAU"]
+export const Camry = [
+  {
+    Name: "2007 Camry",
+    Color: "Brown",
+    Interior: "Leather",
+    Drive: "FWD",
+    Trans: "Automatic",
+    VIN: "XXX",
+    Engine: "XXX",
+    Mileage: 125000,
+    Stock: 25,
+    IMG: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSicayxK-zSQKO2q0gvy_27RbfYY7aOhLUtQ&usqp=CAU",
+  },
 
-export const Corolla = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNGnLD-NZq4k8i-BRFMA_PpkDItLwf60QQVw&usqp=CAU",
-"https://www.autotrader.com/wp-content/uploads/2021/04/2021-toyota-corolla-le-front-right-side.jpg",
-"https://media.ed.edmunds-media.com/toyota/corolla/2023/fe/2023_toyota_corolla_f34_fe_113221_1280.jpg"]
+  {
+    Name: "2007 Camry",
+    Color: "Brown",
+    Interior: "Leather",
+    Drive: "FWD",
+    Trans: "Automatic",
+    VIN: "XXX",
+    Engine: "XXX",
+    Mileage: 125000,
+    Stock: 25,
+    IMG: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT49kT1efPvyvHqPRVaoF67NhM3r4u1geV1Wg&usqp=CAU",
+  },
 
-export const Highlander = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuIdDrazzf1HFI-jry70hykQMGcqzVPvtaMA&usqp=CAU",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ70vThZMqI_ytyQybTNlMKgeuNsaMPB_sXiw&usqp=CAU",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1LVQ7PjRZkFKJf7j0Tqjjkv28LIwGQwM6g&usqp=CAU"]
+  {
+    Name: "2007 Camry",
+    Color: "Brown",
+    Interior: "Leather",
+    Drive: "FWD",
+    Trans: "Automatic",
+    VIN: "XXX",
+    Engine: "XXX",
+    Mileage: 125000,
+    Stock: 25,
+    IMG: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQle3P7smI3CT9kV4U5tW_g_5hh1F1GAXvcEA&usqp=CAU",
+  },
+];
 
-export const Edge = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpDwxL36sdJHD6Sjezto7EUiqH0qiHEUwKRg&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN7VJ6vdwOr_mj4F-wiHA27mRQCO6TzKjNYQ&usqp=CAU",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGtmg7LO-nclBf70i3NRI8L3A2Jd3Er2mY7A&usqp=CAU"]
+export const Corolla = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNGnLD-NZq4k8i-BRFMA_PpkDItLwf60QQVw&usqp=CAU",
+  "https://www.autotrader.com/wp-content/uploads/2021/04/2021-toyota-corolla-le-front-right-side.jpg",
+  "https://media.ed.edmunds-media.com/toyota/corolla/2023/fe/2023_toyota_corolla_f34_fe_113221_1280.jpg",
+];
 
-export const Escape = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4IMpbxGhCr2hRgneq-AxKihclKtmOPSLtOw&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpga0zrnowLo4m7rAgO8_eNnDxLi2gmux_1Q&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6RmPld-WQL5lWYJBIWATiY-cZ1r2HIWsPWw&usqp=CAU"]
+export const Highlander = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuIdDrazzf1HFI-jry70hykQMGcqzVPvtaMA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ70vThZMqI_ytyQybTNlMKgeuNsaMPB_sXiw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQe1LVQ7PjRZkFKJf7j0Tqjjkv28LIwGQwM6g&usqp=CAU",
+];
 
-export const Focus = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuZcrJHbnbfJj6HjavEh_O90Gd7w7Equv7rA&usqp=CAU","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR41ZmCniMUJ8TwIytlX1ppJDuHn1sb0V7MA&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaEBmWJ-5A60MH0M66UvGmjcNlJ_kJY8U5gg&usqp=CAU"]
+export const Edge = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpDwxL36sdJHD6Sjezto7EUiqH0qiHEUwKRg&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN7VJ6vdwOr_mj4F-wiHA27mRQCO6TzKjNYQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGtmg7LO-nclBf70i3NRI8L3A2Jd3Er2mY7A&usqp=CAU",
+];
 
-export const Accord = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSztuIrP17luabwqTyOLiEnebOChRs2ME6jBQ&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxQ_W0mvJEW1DXrY__ZUxUsnRrrkEsz4t36w&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwedwkiaRnSuprUEGiXtz_tkcXCCQx46a6dA&usqp=CAU"]
+export const Escape = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4IMpbxGhCr2hRgneq-AxKihclKtmOPSLtOw&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpga0zrnowLo4m7rAgO8_eNnDxLi2gmux_1Q&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6RmPld-WQL5lWYJBIWATiY-cZ1r2HIWsPWw&usqp=CAU",
+];
 
-export const Civic = ["https://www.topgear.com/sites/default/files/cars-car/image/2020/09/308395_2020_civic_type_r_mmc_launch.jpg", "https://www.motortrend.com/uploads/2021/11/2022-Honda-Civic-Hatchback-Sport-Touring-22.jpg", "https://www.autotrader.com/wp-content/uploads/2020/08/2021-honda-civic-hatchback-front-left-side.jpg"]
+export const Focus = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuZcrJHbnbfJj6HjavEh_O90Gd7w7Equv7rA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR41ZmCniMUJ8TwIytlX1ppJDuHn1sb0V7MA&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaEBmWJ-5A60MH0M66UvGmjcNlJ_kJY8U5gg&usqp=CAU",
+];
+
+export const Accord = [
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSztuIrP17luabwqTyOLiEnebOChRs2ME6jBQ&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxQ_W0mvJEW1DXrY__ZUxUsnRrrkEsz4t36w&usqp=CAU",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwedwkiaRnSuprUEGiXtz_tkcXCCQx46a6dA&usqp=CAU",
+];
+
+export const Civic = [
+  "https://www.topgear.com/sites/default/files/cars-car/image/2020/09/308395_2020_civic_type_r_mmc_launch.jpg",
+  "https://www.motortrend.com/uploads/2021/11/2022-Honda-Civic-Hatchback-Sport-Touring-22.jpg",
+  "https://www.autotrader.com/wp-content/uploads/2020/08/2021-honda-civic-hatchback-front-left-side.jpg",
+];
